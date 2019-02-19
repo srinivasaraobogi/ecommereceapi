cd3d5ad14e4f70edc73f9dbbf33daa6c6e833724
srinivasaraobogi <srinivasaraobogi@gmail.com>
2019-02-19 15:41:45 +0530

    controllers

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::apiResource('/products', 'ProductController');
+Route::prefix('products')->group(function () {
+    Route::apiResource('/{product}/reviews', 'ReviewsController');
+});
